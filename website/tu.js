8d52dbe2e3f6b0a6ced6785e04e6814e54c51eb5
0legLivanov <147545002+0legLivanov@users.noreply.github.com>
2024-02-01 20:42:11 +0300

    1
    1

---
+++ website/tu.js
@@ -0,0 +1,7 @@
+var a=Math.round(Math.random()*3)
+image = new Array();
+image[0]="Icons8-Ios7-Logos-Adobe-Fireworks-Copyrighted.512.png"
+image[1]="Icons8-Ios7-Logos-Adobe-Flash-Copyrighted.512.png"
+image[2]="Icons8-Ios7-Logos-Adobe-Photoshop-Copyrighted.512.png"
+image[3]="vo.webp"
+document.write ("<img src=" + image[a] + ">");
---
+++ website/ut.js
@@ -0,0 +1,7 @@
+var a=Math.round(Math.random()*3)
+image = new Array();
+image[0]="A6ideqU-4bMZBJHa46jW3Dl72eJkfbmt4t8yenImKBVvK0kTmF0xjctABnaLJIm9.jpg"
+image[1]="i.webp"
+image[2]="juwels-cluster.jpg"
+image[3]="juwels-cluster.jpg"
+document.write ("<img src=" + image[a] + ">");
